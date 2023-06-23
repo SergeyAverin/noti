73fbd3431241f3d30ae443756d67c0ca0bc9ca81
SergeyAverin <ct.averin@yandex.ru>
2023-06-23 18:56:02 +0600

    feat: added base struct of project
    Added folders for routes and controllers. Added routes to the note api. Added note controller base class.

---
+++ backend/src/controllers/notes.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+
+class NotesController {
+    get(req:Request, res:Response) {
+        res.send('note list')
+    }
+}
+
+export default  new NotesController();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 
+import routes from './routes/index';
+
 
 const app: Express = express();
 const PORT = Number(process.env.BACKEND_PORT);
 const HOST = process.env.BACKEND_HOST as string;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Noti server');
-});
+// Middleware
+app.use('/api/v1.0', routes);
+
 
 app.listen(PORT, HOST, () => {
   console.log(`[server]: Server is running at http://${HOST}:${PORT}`);
