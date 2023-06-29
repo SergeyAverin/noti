3626767209e66a7cccfc7fccdbdf9007a499062e
SergeyAverin <ct.averin@yandex.ru>
2023-06-29 17:02:50 +0600

    feat(backend): added logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
+import path from 'path';
+
 import express, { Express, Request, Response } from 'express';
+import log4js from 'log4js';
 
 import routes from './routes/index';
 
@@ -7,10 +10,18 @@ const app: Express = express();
 const PORT = Number(process.env.BACKEND_PORT);
 const HOST = process.env.BACKEND_HOST as string;
 
+log4js.configure(path.join(__dirname, '../../conf/log4js.config.json'));
+const logger = log4js.getLogger();
+
 // Middleware
 app.use('/api/v1.0', routes);
-
+app.use(
+  log4js.connectLogger(logger, {
+    level: 'auto',
+  }),
+)
 
 app.listen(PORT, HOST, () => {
-  console.log(`[server]: Server is running at http://${HOST}:${PORT}`);
+  logger.error('err')
+  logger.info(`[server]: Server is running at http://${HOST}:${PORT}`)
 });
