15b19e66697cf27752f6ec8565906bfd409058f8
SergeyAverin <ct.averin@yandex.ru>
2023-07-03 21:43:27 +0600

    docs(backend): added doc string for password utils functions

--- backend/src/utils/passwordUtils.ts
+++ backend/src/utils/passwordUtils.ts
@@ -1,10 +1,21 @@
 import bcrypt from 'bcryptjs'
 
+/**
+ * This is a function that hashed the password.
+ * @param {string} plaintextPassword - The password before hashing.
+ * @returns {Promise<string>} The hashed password.
+ */
 export async function hashPassword(plaintextPassword: string): Promise<string> {
   const hash = await bcrypt.hash(plaintextPassword, 10)
   return hash
 }
 
+/**
+ * This is a function that compares the hashed password with the plaintext Password.
+ * @param {string} plaintextPassword - The string that is compared to the hashed password.
+ * @param {string} hash - The hashed password.
+ * @returns {Promise<boolean>} Is hashed password and plaintextPassword equal.
+ */
 export async function comparePassword(
   plaintextPassword: string,
   hash: string,
