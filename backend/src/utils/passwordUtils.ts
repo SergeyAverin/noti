b671a0dfd357a0b01584a90170bdb14cc594993e
SergeyAverin <ct.averin@yandex.ru>
2023-07-03 21:23:40 +0600

    feat(backend): added compare password function
    This function compares the hashed password with the original password.

--- backend/src/utils/passwordUtils.ts
+++ backend/src/utils/passwordUtils.ts
@@ -4,3 +4,11 @@ export async function hashPassword(plaintextPassword: string): Promise<string> {
   const hash = await bcrypt.hash(plaintextPassword, 10)
   return hash
 }
+
+export async function comparePassword(
+  plaintextPassword: string,
+  hash: string,
+): Promise<boolean> {
+  const result = await bcrypt.compare(plaintextPassword, hash)
+  return result
+}
