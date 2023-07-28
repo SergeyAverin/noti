7cb29019ba4ce8c98a4e08ff4445274568feceb3
SergeyAverin <ct.averin@yandex.ru>
2023-07-29 00:54:36 +0600

    feat(frontend): added user slice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import userReducer from "./features/userSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    userState: userReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
