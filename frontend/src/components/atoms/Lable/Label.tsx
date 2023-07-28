6a3578c654f4af39cb685474acfb1b3587bcafee
SergeyAverin <ct.averin@yandex.ru>
2023-07-28 22:40:11 +0600

    feat(frontend): added label component

---
+++ frontend/src/components/atoms/Lable/Label.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Label = styled.label`
+  color: #f5f5f5;
+  position: absolute;
+  top: 50%;
+  transform: translate(0px, -50%);
+  font-size: 24px;
+  transition: all 0.2s ease;
+  z-index: 1;
+`;
