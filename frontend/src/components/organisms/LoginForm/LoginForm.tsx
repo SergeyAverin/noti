613fcf08c2664bacfec738d49730322210631c33
SergeyAverin <ct.averin@yandex.ru>
2023-07-28 23:50:36 +0600

    feat(frontend): added validation in login form

--- frontend/src/components/organisms/LoginForm/LoginForm.tsx
+++ frontend/src/components/organisms/LoginForm/LoginForm.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm, Controller, Control, FieldValues } from "react-hook-form";
 
 import { LoginFormStyled } from "./loginFormStyled";
-import { Margin, SubmitButton } from "@atoms/index";
+import { Margin, SubmitButton, ErrorText } from "@atoms/index";
 import { InputLabelController } from "@molecules/InputLabel";
 
 type FormValues = {
@@ -11,9 +11,13 @@ type FormValues = {
 };
 
 export const LoginForm: React.FC = () => {
-  const { control, handleSubmit } = useForm<FormValues>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
   const onSubmit = (data: FormValues) => {
-    console.log('data');
+    console.log("data");
     console.log(data);
   };
   return (
@@ -22,6 +26,13 @@ export const LoginForm: React.FC = () => {
         text="Email"
         control={control}
         name="email"
+        rules={{
+          required: "Email is required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Invalid email address",
+          },
+        }}
       />
       <Margin mt={15}>
         <InputLabelController
@@ -29,10 +40,16 @@ export const LoginForm: React.FC = () => {
           attrType="password"
           control={control}
           name="password"
+          rules={{ required: "Password is required" }}
         />
       </Margin>
       <Margin mt={15}>
         <SubmitButton value="login" />
+      </Margin>
+      <Margin mt={30}>
+        {errors.email && <ErrorText>{errors.email.message}</ErrorText>}
+        <br />
+        {errors.password && <ErrorText>{errors.password.message}</ErrorText>}
       </Margin>
     </LoginFormStyled>
   );
