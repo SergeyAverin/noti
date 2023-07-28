d1b17b6a9248f34e4f75af7d9c83893e7948e560
SergeyAverin <ct.averin@yandex.ru>
2023-07-28 17:27:20 +0600

    feat(frontend): added redux store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import "normalize.css";
 
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 
 import GlobalStyleStyled from "./styles/globalStyle.styled";
 import { darkTheme } from "./styles/theme";
+import { store } from "./redux/store";
 
-const App = () => {
+import "normalize.css";
+
+const App: React.FC = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <div className="App">
-        <GlobalStyleStyled />
-        <h1>React Template</h1>
-      </div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={darkTheme}>
+        <div className="App">
+          <GlobalStyleStyled />
+          <h1>React Template</h1>
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
