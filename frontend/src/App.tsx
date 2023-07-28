d076882cc43e92e0729b09e21814b80c39529843
SergeyAverin <ct.averin@yandex.ru>
2023-07-28 16:23:56 +0600

    feat(frontend): added global style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyleStyled from "./styles/globalStyle.styled";
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyleStyled />
       <h1>React Template</h1>
     </div>
   );
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  color: {
+    white: "#FCFCFC",
+    bg: "#1D1D1D",
+    grey: "#272727",
+    black: "#111111",
+    grean: "#00BC5E",
+    red: "#E76161",
+  },
+};
