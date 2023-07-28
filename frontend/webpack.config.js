1c485deb8ea75fbaec590de5b218dba7480d45b8
SergeyAverin <ct.averin@yandex.ru>
2023-07-28 22:38:56 +0600

    fix(frontend): fixed molecules alias

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -32,7 +32,7 @@ const config = {
     extensions: [".ts", ".tsx", ".js"],
     alias: {
       "@atoms": path.resolve(__dirname, 'src/components/atoms'),
-      "@molecules": path.resolve(__dirname, 'src/components/atoms'),
+      "@molecules": path.resolve(__dirname, 'src/components/molecules'),
       "@organisms": path.resolve(__dirname, 'src/components/organisms'),
       "@templates": path.resolve(__dirname, 'src/components/templates'),
       "@pages": path.resolve(__dirname, 'src/components/pages'),
