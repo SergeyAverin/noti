b2ec285df02db172a1258a79bc56b56b984efb1d
SergeyAverin <ct.averin@yandex.ru>
2023-08-01 17:32:56 +0600

    feat(frontend): added logout page

--- frontend/src/components/pages/LogoutPage.tsx
+++ frontend/src/components/pages/LogoutPage.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { withAuth } from "../../hocs/withAuth";
 
 const LogoutPage: React.FC = () => {
+  const navigate = useNavigate();
+  useEffect(()=> {
+    localStorage.removeItem('token')
+    navigate('/auth/login')
+  })
   return <div>logout</div>;
 };
 
-export default LogoutPage;
+export default withAuth(LogoutPage);
