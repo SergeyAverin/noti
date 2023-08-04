e7af67dc60e6f176643ff8c1a1bb195ec1cdc053
SergeyAverin <ct.averin@yandex.ru>
2023-08-04 18:24:49 +0600

    feat(added-libraries-controllers): Added controllers for trash and bookmarks

--- backend/src/controllers/notes.controller.ts
+++ backend/src/controllers/notes.controller.ts
@@ -1,9 +1,39 @@
 import { Request, Response } from 'express'
+import { NoteService } from '../services/note.service'
+import { StatusCodes } from 'http-status-codes'
 
-class NotesController {
-  get(req: Request, res: Response) {
-    res.send('note list')
-  }
+export const addTrash = async (req: Request, res: Response) => {
+  const noteService = new NoteService()
+  const slug = req.params.slug
+  await noteService.addTrash(slug)
+  res.status(StatusCodes.OK).send({
+    message: `Note ${slug} added in trash`,
+  })
 }
 
-export default new NotesController()
+export const removeTrash = async (req: Request, res: Response) => {
+  const noteService = new NoteService()
+  const slug = req.params.slug
+  await noteService.removeTrash(slug)
+  res.status(StatusCodes.OK).send({
+    message: `Note ${slug} removed from trash`,
+  })
+}
+
+export const addBookmark = async (req: Request, res: Response) => {
+  const noteService = new NoteService()
+  const slug = req.params.slug
+  await noteService.addBookmark(slug)
+  res.status(StatusCodes.OK).send({
+    message: `Note ${slug} added in bookmark`,
+  })
+}
+
+export const removeBookmark = async (req: Request, res: Response) => {
+  const noteService = new NoteService()
+  const slug = req.params.slug
+  await noteService.removeBookmark(slug)
+  res.status(StatusCodes.OK).send({
+    message: `Note ${slug} removed in bookmark`,
+  })
+}
--- backend/src/routes/notes.routes.ts
+++ backend/src/routes/notes.routes.ts
@@ -1,11 +1,22 @@
 import express, { Router } from 'express'
 
-import notesControllerfrom from '../controllers/notes.controller'
+import {
+  addBookmark,
+  addTrash,
+  removeBookmark,
+  removeTrash,
+} from '../controllers/notes.controller'
 import { authorizationRequireMiddleware } from '../middlewares/authorizationRequireMiddleware'
 
 const noteRoute: Router = express.Router()
 
 noteRoute.use(authorizationRequireMiddleware)
-noteRoute.route('').get((req, res) => notesControllerfrom.get(req, res))
+// noteRoute.route('').get((req, res) => notesControllerfrom.get(req, res))
+noteRoute.route('/:slug/trash').post((req, res) => addTrash(req, res))
+noteRoute.route('/:slug/trash').delete((req, res) => removeTrash(req, res))
+noteRoute.route('/:slug/bookmark').post((req, res) => addBookmark(req, res))
+noteRoute
+  .route('/:slug/bookmark')
+  .delete((req, res) => removeBookmark(req, res))
 
 export default noteRoute
