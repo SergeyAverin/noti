4842e38c543efd1544db4d03601175408ce5e5a7
SergeyAverin <ct.averin@yandex.ru>
2023-08-07 13:45:57 +0600

    feat(frontend): added bookmark query in bookmark component

--- frontend/src/components/molecules/Bookmar/Bookmar.tsx
+++ frontend/src/components/molecules/Bookmar/Bookmar.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+import { useGetBookmarkQuery } from "@redux/api/libraryApi";
 import { ToggleMenu, Margin } from "@atoms/index";
 
-import BookmarkIcon from '@public/BookmarkIcon.svg'
+import BookmarkIcon from "@public/BookmarkIcon.svg";
 
-export const Bookmark: React.FC = () =>{
-    return (
-        <ToggleMenu text="Bookmark"  icon={<BookmarkIcon />}>
-            <Margin ml={30}>
-                <ToggleMenu text="tesx">d</ToggleMenu>
-            </Margin>
-        </ToggleMenu>
-    )
-}
+export const Bookmark: React.FC = () => {
+  const { data, isLoading, isError } = useGetBookmarkQuery();
+  return (
+    <ToggleMenu text="Bookmark" icon={<BookmarkIcon />}>
+      <Margin ml={30}>
+        {!isLoading &&
+          data &&
+          data.map((note) => <li key={note.slug}>{note.title}</li>)}
+      </Margin>
+    </ToggleMenu>
+  );
+};
