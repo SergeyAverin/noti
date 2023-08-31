83822bb83b50320c60fe812895b01a6ab352319e
SergeyAverin <ct.averin@yandex.ru>
2023-08-31 06:42:08 -1200

    feat(frontend): added formatdate in data created component

--- frontend/src/components/molecules/DataCreated/DataCreated.tsx
+++ frontend/src/components/molecules/DataCreated/DataCreated.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { DataCreatedStyled, DataCreatedMoreStyled } from "./DataCreatedStyled";
+import { formatDate } from "@utils/formatedDate";
 
 interface IDataCreatedProps {
   dataCreated: Date;
@@ -10,14 +11,6 @@ export const DataCreated: React.FC<IDataCreatedProps> = ({
   dataCreated,
   dataEdited,
 }) => {
-  function formatDate(date: Date) {
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
-  }
   return (
     <DataCreatedStyled>
       Last edit at {formatDate(dataEdited)}
