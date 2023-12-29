6175ca26c98c372347ea69189f32dfcac8e0334e
SergeyAverin <ct.averin@yandex.ru>
2023-12-29 15:15:52 +0600

    feat(frontend): added theme from redux in style component theme providor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 
 import { ThemeProvider } from "styled-components";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import GlobalStyleStyled from "./styles/globalStyle.styled";
-import { darkTheme } from "./styles/theme";
-import { store } from "./redux/store";
+
+import { themeSelector } from "@redux/selectors/theme";
+import { store } from "@redux/store";
 import MainRouter from "./router";
 
 import "normalize.css";
 
+
+const Theme: React.FC<PropsWithChildren> = ({ children }) => {
+  const theme = useSelector(themeSelector)
+  return (
+    <ThemeProvider theme={theme}>
+      { children }
+    </ThemeProvider>
+  )
+}
+
 const App: React.FC = () => {
+
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <ThemeProvider theme={darkTheme}>
+        <Theme>
           <GlobalStyleStyled />
             <DndProvider backend={HTML5Backend}>
               <MainRouter />
             </DndProvider>
-        </ThemeProvider>
+        </Theme>
       </BrowserRouter>
     </Provider>
   );
