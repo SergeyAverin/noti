c9ce6e57f9cebcebe81f17d49ef808818cfe1839
SergeyAverin <ct.averin@yandex.ru>
2024-01-10 01:37:54 +0600

    build(frontend): installed jest transformer svg

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,6 +3,24 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  modulePaths: ["src"],
+  moduleDirectories: ["node_modules"],
+  moduleNameMapper: {
+    "^@atoms(.*)$": "<rootDir>/src/components/atoms$1",
+    "^@molecules(.*)$": "<rootDir>/src/components/molecules$1",
+    "^@organisms(.*)$": "<rootDir>/src/components/organisms$1",
+    "^@templates(.*)$": "<rootDir>/src/components/templates$1",
+    "^@pages(.*)$": "<rootDir>/src/components/pages$1",
+
+    "^@utils(.*)$": "<rootDir>/src/utils$1",
+    "^@hooks(.*)$": "<rootDir>/src/hooks$1",
+    "^@hocs(.*)$": "<rootDir>/src/hocs$1",
+    "^@public(.*)$": "<rootDir>/public$1",
+  },
+
+  transform: {
+    "^.+\\.svg$": "jest-transformer-svg",
+  },
 };
 
 export default config;
