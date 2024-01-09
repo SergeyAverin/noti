eff9e39f1f8d02f0658e6474ea70f08cbbd9cc43
SergeyAverin <ct.averin@yandex.ru>
2024-01-10 01:36:50 +0600

    fix(frontend): fixed wrapper test

--- frontend/src/components/atoms/Wrapper/Wrapper.tsx
+++ frontend/src/components/atoms/Wrapper/Wrapper.tsx
@@ -5,7 +5,7 @@ import { WrapperStyled } from "./WrapperStyled";
 /** This component adds indents to the header and sidebar*/
 export const Wrapper: React.FC<React.PropsWithChildren> = (props) => {
   return (
-    <div date-testid="wrapper">
+    <div data-testid="wrapper">
       <WrapperStyled {...props} />
     </div>
   );
