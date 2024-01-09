a9ae582a2be719ad20b114a45d2bb7bdd3e6f618
SergeyAverin <ct.averin@yandex.ru>
2024-01-09 17:25:41 +0600

    test(frontend): added link bg component test

---
+++ frontend/src/components/atoms/LinkBG/LinkBG.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import { ThemeProvider } from "styled-components";
+import React from "react";
+
+import { LinkBG } from "./LinkBG";
+import { darkTheme } from "../../../styles/darkTheme";
+import { BrowserRouter, useLocation } from "react-router-dom";
+
+describe("LinkBG component", () => {
+  it("Should render LinkBG and changed URL after click on button", async () => {
+    render(
+      <BrowserRouter>
+        <ThemeProvider theme={darkTheme}>
+          <LinkBG href="/test" text="test" />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+
+    const buttonElement = screen.getByText("test");
+
+    expect(window.location.pathname).not.toEqual("/test");
+
+    await userEvent.click(buttonElement);
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(window.location.pathname).toEqual("/test");
+  });
+});
