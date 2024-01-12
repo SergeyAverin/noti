86287e4b9170ba94c062fde42b7e8cea478bf97f
SergeyAverin <ct.averin@yandex.ru>
2024-01-12 17:08:15 +0600

    docs(frontend): added formatting tools component stories

--- frontend/src/components/molecules/editor/FormattingTools/FormattingToolsContainer.tsx
+++ frontend/src/components/molecules/editor/FormattingTools/FormattingToolsContainer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FormattingToolsPresentational } from "./FormattingToolsPresentational";
 import { useExecCommand } from "@hooks/useExecCommand";
 
-/**This component is tools in text editor toolbar.
+/** This component is tools in text editor toolbar.
  *  This component have a buttons to work with text format in text editor.
  */
 export const FormattingToolsContainer: React.FC = () => {
