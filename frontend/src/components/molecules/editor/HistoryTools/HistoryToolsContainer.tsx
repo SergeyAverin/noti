152f83917b35e59809ffc5c8c388d93c81acb7e7
SergeyAverin <ct.averin@yandex.ru>
2024-01-12 17:02:45 +0600

    docs(frontend): added history tools component stories

--- frontend/src/components/molecules/editor/HistoryTools/HistoryToolsContainer.tsx
+++ frontend/src/components/molecules/editor/HistoryTools/HistoryToolsContainer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { HistoryToolsPresentational } from "./HistoryToolsPresentational";
 import { useExecCommand } from "@hooks/useExecCommand";
 
-/**This component is tools in text editor toolbar
- *  This component have a buttons to work with history in text editor
+/**This component is tools in text editor toolbar.
+ *  This component have a buttons to work with history in text editor.
  */
 export const ListToolsContainer: React.FC = () => {
   const execCommand = useExecCommand();
