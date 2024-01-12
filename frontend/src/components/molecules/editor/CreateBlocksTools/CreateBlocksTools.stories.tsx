b1fc2ee760ad6d035e3cfe389eb54778234b0073
SergeyAverin <ct.averin@yandex.ru>
2024-01-12 18:17:05 +0600

    docs(frontend): added craete blocks tools component stories

---
+++ frontend/src/components/molecules/editor/CreateBlocksTools/CreateBlocksTools.stories.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+
+import CreateBlockTools from "./index";
+import { Flex, Margin } from "@atoms/index";
+import styled from "styled-components";
+
+export default {
+  title: "molecules/editor/CreateBlockTools",
+  component: CreateBlockTools,
+  tags: ["autodocs"],
+} as Meta;
+
+const TestEditor = styled.div.attrs({
+  contentEditable: true,
+  placeholder: "Write text...",
+})`
+  width: 100%;
+  height: 250px;
+  overflow-y: auto;
+  outline: none;
+  border: 1px solid black;
+  padding: 15px;
+  &:empty:before {
+    content: attr(placeholder);
+    pointer-events: none;
+    display: block;
+    color: ${(props) => props.theme.propertyColor.fg.grey};
+  }
+`;
+
+const Template: StoryFn = () => (
+  <>
+    <Flex alignItems="center" justifyContent="flex-start">
+      <CreateBlockTools />
+    </Flex>
+    <Margin mt={30}>
+      <TestEditor />
+    </Margin>
+  </>
+);
+export const Default = Template.bind({});
